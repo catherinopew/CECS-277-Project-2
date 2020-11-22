addd1b4b99cba1ecef51897bb2dee3f4d7798fda
cat <catherine.bui02@student.csulb.edu>
2020-11-21 21:03:41 -0800

    added new instance variables

--- Item.java
+++ Item.java
@@ -2,12 +2,18 @@
 public class Item {
     /** The name of the item */
     private String name;
+    /** The value (in gold) of an item */
+    private int value;
+    /** The type of an item */
+    private char type;
 
     /** Constructs an item
      * @param n the item's name
      */
-    public Item(String n) {
+    public Item(String n, int v, char t) {
         name = n;
+        value = v;
+        type = t;
     }
 
     /** Retrieves the name of the item
