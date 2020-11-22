804857c6b6d1fa430967e336e3475b03008865d3
jacobsunia <70661851+jacobsunia@users.noreply.github.com>
2020-11-21 19:49:15 -0800

    Added attack to each enemy type

--- Goblin.java
+++ Goblin.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class Goblin extends Enemy{
     // instance variables - replace the example below with your own
     public Goblin(Item i){
         super("Goblin", 6, i);
+    }
+    @Override
+    public String attack(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(7) + 1;
+        e.takeDamage(damage);
+        return getName() + " attacks " + e.getName() + " for " + damage + " damage.";
     }
 }
--- Orc.java
+++ Orc.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class Orc extends Enemy{
     // instance variables - replace the example below with your own
     public Orc(Item i){
         super("Froglok", 9, i);
+    }
+    @Override
+    public String attack(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(7) + 1;
+        e.takeDamage(damage);
+        return getName() + " attacks " + e.getName() + " for " + damage + " damage.";
     }
 }
