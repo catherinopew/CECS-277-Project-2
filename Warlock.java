14c128692946ce0b482f1de9f826e9d26ce09582
jacobsunia <70661851+jacobsunia@users.noreply.github.com>
2020-11-21 19:25:26 -0800

    Updated Warlock method, acts as a magical enemy

--- Warlock.java
+++ Warlock.java
@@ -7,8 +7,56 @@ public class Warlock extends Enemy_Decorator {
     @Override
     public String attack( Entity e ){
         Random rand = new Random();
-        int damage = rand.nextInt(7) + 6;
+        int random = rand.nextInt(3) + 1;
+        if (random == 1) {
+            return magicMissile(e);
+        }
+        else if (random == 2) {
+            return fireball(e);
+        }
+        else {
+            return thunderclap(e);
+        }
+    }
+    
+    /** Uses a fireball on the hero
+     * @param e the hero being attacked
+     * @return String the attack message
+     */
+    @Override
+    public String fireball(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(7) + 1;
         e.takeDamage(damage);
-        return getName() + " attacks " + e.getName() + " for " + damage + " damage.";
+
+        return getName() + " hits " + e.getName() + " with a Fireball for "
+        + damage + " damage.";
+    }
+    
+    /** Uses a magic missile on the hero
+     * @param e the hero being attacked
+     * @return String the attack message
+     */
+    @Override
+    public String magicMissile(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(7) + 1;
+        e.takeDamage(damage);
+
+        return getName() + " hits " + e.getName() + " with a Magic Missle for "
+        + damage + " damage.";
+    }
+    /** Uses thunderclap on the hero
+     * @param e the hero being attacked
+     * @return String the attack message
+     */
+    @Override
+    public String thunderclap(Entity e) {
+        Random rand = new Random();
+        int damage = rand.nextInt(7) + 1;
+        e.takeDamage(damage);
+
+        return getName() + " zaps " + e.getName() + " with Thunderclap for "
+        + damage + " damage.";
     }
 }
