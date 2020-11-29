71004115f730b323509592dfddfa66cf425af41f
jacobsunia <70661851+jacobsunia@users.noreply.github.com>
2020-11-29 14:40:18 -0800

    Created a occurence of the warriors/warlock while creating the factory.

--- EnemyGenerator.java
+++ EnemyGenerator.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
@@ -5,63 +6,70 @@ import java.io.FileNotFoundException;
 import java.util.Random;
 
 /** EnemyGenerator class that generates an enemy */
-public class EnemyGenerator {
+public class EnemyGenerator{
     /** An ArrayList of the enemies */
     private ArrayList <Enemy> enemyList = new ArrayList <Enemy> ();
     /** A randomly generated item */
     private ItemGenerator ig;
+    /** A instance of the enemyGenerator */
+    private static EnemyGenerator instance = null;
 
-    /** Reads from the EnemyList.txt file 
-     * Constructs and adds each enemy to the ArrayList, enemyList
+    /** Constructs and adds each enemy to the ArrayList, enemyList
      * @param ig item to be generated 
      */
     public EnemyGenerator(ItemGenerator ig) {
         this.ig = ig;
-        try {
-            Scanner read = new Scanner(new File("EnemyList.txt"));
-            while(read.hasNextLine()) {
-                String line = read.nextLine();
-                String [] token = line.split(",");
-                Random rand = new Random(); 
-                int randHealth = rand.nextInt(3) + 1;
-
-                if (token[2].equals("m")) { //if the enemy is magical, construct a new magical enemy
-                    enemyList.add(new MagicalEnemy(token[0], 
-                    Integer.parseInt(token[1]) + randHealth, ig.generateItem())); 
-                }
-                else { //otherwise, construct a physical enemy
-                    enemyList.add(new Enemy(token[0], 
-                    Integer.parseInt(token[1]) + randHealth, ig.generateItem())); 
-                }
+        Random ran = new Random();
+        int num = ran.nextInt(4)+1;
+        int i = 0;
+        while(i != 10){
+            num = ran.nextInt(4)+1;
+            if (num == 1){
+                enemyList.add(new Troll(ig.generateItem()));
+            } else if (num == 2){
+                enemyList.add(new Froglok(ig.generateItem()));
+            } else if (num == 3){
+                enemyList.add(new Orc(ig.generateItem()));
+            } else {
+                enemyList.add(new Goblin(ig.generateItem()));
             }
-            read.close();
-        } 
-        catch (FileNotFoundException e) {
-            System.out.println("File Not Found - place in project folder");
+            i++;
         }
     }
+    /** getInstance ensures that there is only one instance of enemyGenerator
+    *   @param ig is used to add to the instance of the enemyGenerator
+    *   @return the instance of the enemy generator 
+    */
     public static EnemyGenerator getInstance(ItemGenerator ig){
         if (instance == null){
             instance = new EnemyGenerator(ig);
         }
         return instance; 
     }
+
     /** Randomly generates an enemy from the ArrayList
      * @param level the level in which the game is on
      * @return Enemy a randomly generated enemy
      */
     public Enemy generateEnemy(int level) {
-        int random = (int)(Math.random() * enemyList.size());
-
-        if (enemyList.get(random) instanceof MagicalEnemy) {
-            Enemy magical = new MagicalEnemy(enemyList.get(random).getName(),
-            enemyList.get(random).getMaxHP(), enemyList.get(random).getItem());
-            return magical;
-        }
-        else {
-            Enemy physical = new Enemy(enemyList.get(random).getName(),
-            enemyList.get(random).getMaxHP(), enemyList.get(random).getItem());
+        int ran = (int)(Math.random() * enemyList.size());
+        Random rand = new Random();
+        int occurence = rand.nextInt(10)+1; 
+        System.out.println(occurence);
+        
+        if (occurence < 3) {
+            Enemy warrior = new Warrior(enemyList.get(ran));            
+            return warrior;
+        } else if (occurence >= 3 && occurence < 5){
+            Enemy warlock = new Warlock(enemyList.get(ran));
+            return warlock;
+        } else {
+            Enemy physical = new Enemy(enemyList.get(ran).getName(), enemyList.get(ran).getMaxHP(), enemyList.get(ran).getItem());
+            if (level > 1){
+                physical = new Enemy(enemyList.get(ran).getName(), enemyList.get(ran).getMaxHP()+4, enemyList.get(ran).getItem());
+             
+            }
             return physical;
-        }
+        } 
     }
 }
