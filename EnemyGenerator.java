7b9960cc2444543249c26058e40c9b493e6a879b
cat <catherine.bui02@student.csulb.edu>
2020-12-05 16:56:00 -0800

    javadocs & comments
    made neater too

--- EnemyGenerator.java
+++ EnemyGenerator.java
@@ -1,7 +1,4 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Random;
 
 /** EnemyGenerator class that generates an enemy */
@@ -14,20 +11,20 @@ public class EnemyGenerator{
     private static EnemyGenerator instance = null;
 
     /** Constructs and adds each enemy to the ArrayList, enemyList
-     * @param ig item to be generated 
+     * @param ig enemy's item to be generated 
      */
     public EnemyGenerator(ItemGenerator ig) {
         this.ig = ig;
         Random ran = new Random();
         int num = 0;
 
-        for (int i = 0; i < 10; i++) {
-            num = ran.nextInt(4)+1;
-            if (num == 1){
+        for (int i = 0; i < 10; i++) { //creates a randomized enemy each time
+            num = ran.nextInt(4) + 1; 
+            if (num == 1) {
                 enemyList.add(new Troll(ig.generateItem()));
-            } else if (num == 2){
+            } else if (num == 2) {
                 enemyList.add(new Froglok(ig.generateItem()));
-            } else if (num == 3){
+            } else if (num == 3) {
                 enemyList.add(new Orc(ig.generateItem()));
             } else {
                 enemyList.add(new Goblin(ig.generateItem()));
@@ -36,9 +33,9 @@ public class EnemyGenerator{
     }
 
     /** getInstance ensures that there is only one instance of enemyGenerator
-    *   @param ig is used to add to the instance of the enemyGenerator
-    *   @return the instance of the enemy generator 
-    */
+     * @param ig is used to add to the instance of the enemyGenerator
+     * @return the instance of the enemy generator 
+     */
     public static EnemyGenerator getInstance(ItemGenerator ig){
         if (instance == null){
             instance = new EnemyGenerator(ig);
@@ -47,31 +44,32 @@ public class EnemyGenerator{
     }
 
     /** Randomly generates an enemy from the ArrayList
+     * and adds stacks to it depending on the level of the game.
      * @param level the level in which the game is on
      * @return Enemy a randomly generated enemy
      */
     public Enemy generateEnemy(int level) {
         Random rand = new Random();
-        int occurrence = 0;
         int ran = (int)(Math.random() * enemyList.size());
-        int counter = 2;
+        int counter = 2; //health boost
+        int occurrence = 0;
 
         Enemy physical = new Enemy(enemyList.get(ran).getName(), 
         enemyList.get(ran).getMaxHP()+(counter * level), 
-        enemyList.get(ran).getItem()); //this gets reset each time
+        enemyList.get(ran).getItem()); //creates a random base enemy
 
-        if (level > 1) {
+        if (level > 1) { //stack if level greater than 1
             occurrence = rand.nextInt(2) + 1;
             int stack = 0;
 
-            if (occurrence == 1) {
+            if (occurrence == 1) { //stack Warrior
                 while (stack != level - 1) {
                     physical = new Warrior(physical);
                     stack++;
                 }
             }
             else {
-                while (stack != level - 1) {
+                while (stack != level - 1) { //stack Warlock
                     physical = new Warlock(physical);
                     stack++;
                 }
