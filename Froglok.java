1fc6190feb0a166aed97fa690b492e7bc1ba388d
cat <catherine.bui02@student.csulb.edu>
2020-12-05 16:58:51 -0800

    javadocs
    made neater too

--- Froglok.java
+++ Froglok.java
@@ -1,13 +1,14 @@
 import java.util.Random;
 
 /** Froglok is a enemy base class that constructs a Froglok character for the game */
-public class Froglok extends Enemy{
+public class Froglok extends Enemy {
     /** Froglok class is a constructor class for Froglok that passes in the superclass Enemy
      * @param i is the current item that the Froglok is holding  
-    */
+     */
     public Froglok(Item i){
         super("Froglok", 2, i);
     }
+    
     /** Attacks the hero for a random amount of damage
      * @param e the hero who is being attacked
      * @return String the attack message
