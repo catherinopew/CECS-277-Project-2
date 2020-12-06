71ec4ec8d777600e51ce070232fc0d3adc39e4c9
cat <catherine.bui02@student.csulb.edu>
2020-12-05 16:59:52 -0800

    javadocs
    made neater too

--- Goblin.java
+++ Goblin.java
@@ -5,9 +5,10 @@ public class Goblin extends Enemy{
     /** Goblin class is a constructor class for Goblin that passes in the superclass Enemy
      * @param i is the current item that the Goblin is holding  
     */
-    public Goblin(Item i){
+    public Goblin(Item i) {
         super("Goblin", 2, i);
     }
+
     /** Attacks the hero for a random amount of damage
      * @param e the hero who is being attacked
      * @return String the attack message
