a7d7ef2833870cc477ec905a631e460a5684848c
cat <catherine.bui02@student.csulb.edu>
2020-12-05 17:23:27 -0800

    javadocs
    made neater too

--- Warrior.java
+++ Warrior.java
@@ -1,13 +1,14 @@
 import java.util.Random;
-/** Warrior class is a decorator to all enemy types giving the enemies*/
+/** Warrior class is a decorator to all enemy types giving the enemies */
 public class Warrior extends Enemy_Decorator {
     /** Warrior is the constructor that takes in the enemy to get the enemy object
      *  of the superclass
      *  @param e is the enemy type that will be passed into Warlock
-    */
+     */
     public Warrior ( Enemy e ){
-        super(e, " Warlock " + e.getName(), e.getMaxHP() + 1, e.getItem());
+        super(e, "Warrior " + e.getName(), e.getMaxHP() + 1, e.getItem());
     }
+
     /** Attacks the hero for a random amount of damage
      * @param e the hero who is being attacked
      * @return String the attack message
@@ -17,6 +18,7 @@ public class Warrior extends Enemy_Decorator {
         Random rand = new Random();
         int damage = rand.nextInt(7) + 4;
         e.takeDamage(damage);
-        return getName() + " attacks " + e.getName() + " for " + damage + " damage.";
+        return getName() + " attacks " + e.getName() + 
+        " for " + damage + " damage.";
     }
 }
